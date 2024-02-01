4535f9473b09419b7f04329b13fa7aec0f95ba91
script-nomad <truedemon@riseup.net>
2024-02-01 15:51:26 -0800

    Pelican setup

---
+++ nomadsec/pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Michael Stringer'
+SITENAME = 'Nomad Security'
+SITEURL = "https://nomadsec.io/"
+
+PATH = "content"
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Nomad Security", "https://nomadsec.io/")
+)
+
+# Social widget
+SOCIAL = (
+    ("GitHub", "https://github.com/Script-Nomad"),
+    ("LinkedIn", "https://www.linkedin.com/in/leestringer1/"),
+    ("Twitter/X", "https://twitter.com/Script_Nomad")
+
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
